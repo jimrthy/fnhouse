ba9e4ae824cbbd6dc6f65de7f4b91be95930b5f0
James Gatannah <james.gatannah@gmail.com>
2015-09-06 17:44:06 +0000

    Bump schema to 1.0.0, get it to compile again
    Also deploy to my own repo so I can start testing.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject prismatic/fnhouse "0.1.3-SNAPSHOT"
+(defproject jimrthy/fnhouse "0.1.3-SNAPSHOT"
   :description "Transform lightly-annotated functions into a full-fledged web service"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :url "https://github.com/Prismatic/fnhouse"
-  :dependencies [[prismatic/plumbing "0.4.3"]]
+  :dependencies [[prismatic/plumbing "0.4.4" :exclusions [prismatic/schema]]
+                 [prismatic/schema "1.0.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
                    :global-vars {*warn-on-reflection* true}}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
